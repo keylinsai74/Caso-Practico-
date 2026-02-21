14b3ce7d0ca2adc798ade7515fc6d10f0e0a7043
Ridiell <158251650+Ridiell@users.noreply.github.com>
2026-02-20 20:36:43 -0600

    Modelo reserva y repository

---
+++ WebApplicationAPP/Models/Reserva.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplicationAPP.Models
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public string NombreDelAsociado { get; set; }
+        public string Identificacion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public decimal MontoTotal { get; set; }
+        public DateTime FechaDelServicio { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public int IdServicio { get; set; }
+
+
+        //Muestra los datos en la busqueda
+        public virtual Servicio Servicio { get; set; }
+    }
+}
---
+++ WebApplicationAPP/Repositories/ICooperativaRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Repositories
+{
+    public interface ICooperativaRepository
+    {
+        // Servicios
+        Task<IEnumerable<Servicio>> GetAllServicios();
+        Task<Servicio> GetServicioById(int id);
+        Task AddServicio(Servicio servicio);
+        Task UpdateServicio(Servicio servicio);
+
+
+        // Reservas
+        Task AddReserva(Reserva reserva);
+        Task<IEnumerable<Reserva>> GetReservas(int? idServicio = null);
+        Task<Reserva> GetReservaById(int id);
+    }
+}
