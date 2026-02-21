fe30b84a88b2f9f876cf7572a7ed89d59951b98d
Ridiell <158251650+Ridiell@users.noreply.github.com>
2026-02-20 20:12:12 -0600

    Modelo servicio

---
+++ WebApplicationAPP/Models/Servicio.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplicationAPP.Models
+{
+    public class Servicio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Monto { get; set; }
+        public decimal IVA { get; set; }
+        public int AreaServicio { get; set; }
+        public string Encargado { get; set; }
+        public string Sucursal { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public DateTime? FechaDeModificacion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
+
+//MODELO SERVICIO RIDIELL
